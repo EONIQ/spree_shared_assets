8110e7b449a6bab967cd158df3b7fd34356bb526
chung.triniti@gmail.com <johndoe@example.com>
2016-08-29 18:58:45 +0800

    Remove asset generation

--- lib/generators/spree_shared_assets/install/install_generator.rb
+++ lib/generators/spree_shared_assets/install/install_generator.rb
@@ -3,17 +3,17 @@ module SpreeSharedAssets
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_shared_assets\n"
+        # append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_shared_assets\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_shared_assets\n", :before => /\*\//, :verbose => true
+        # inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_shared_assets\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_shared_assets'
       end
-
+      
       def run_migrations
          res = ask "Would you like to run the migrations now? [Y/n]"
          if res == "" || res.downcase == "y"
