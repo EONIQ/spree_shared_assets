5d77a7207438663424f0029d137cfe65330a9913
chung.triniti@gmail.com <johndoe@example.com>
2016-08-29 19:31:44 +0800

    Add namespace to table name

--- db/migrate/20100602105058_create_assets_shares_table.rb
+++ db/migrate/20100602105058_create_assets_shares_table.rb
@@ -1,16 +1,16 @@
 class CreateAssetsSharesTable < ActiveRecord::Migration
   def self.up
-    create_table :assets_shares do |t|
+    create_table :spree_assets_shares do |t|
       t.references :shareable, :polymorphic => true
       t.references :asset
     end
-    add_index :assets_shares, :shareable_id
-    add_index :assets_shares, :shareable_type
+    add_index :spree_assets_shares, :shareable_id
+    add_index :spree_assets_shares, :shareable_type
   end
 
   def self.down
-    remove_index :assets_shares, :shareable_id
-    remove_index :assets_shares, :shareable_type
-    drop_table :assets_shares
+    remove_index :spree_assets_shares, :shareable_id
+    remove_index :spree_assets_shares, :shareable_type
+    drop_table :spree_assets_shares
   end
 end
