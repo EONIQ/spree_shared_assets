ac3e2ebf45e8eeced4651cf6a60bba73278ee7e3
chung.triniti@gmail.com <johndoe@example.com>
2016-08-29 18:51:23 +0800

    Fix other includes

--- app/models/variant_decorator.rb
+++ app/models/variant_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Variant.class_eval do
 
-  has_many :assets_shares, :dependent => :destroy, :as => :shareable, :include => :asset
-  has_many :images, :source => :asset, :foreign_key => "asset_id", :through => :assets_shares, :class_name => "Image", :dependent => :destroy, :order => "assets.position ASC"
+  has_many :assets_shares, -> { includes(:asset) }, dependent: :destroy, as: :shareable
+  has_many :images, -> { order('assets.position ASC') }, source: :asset, foreign_key: 'asset_id', through: :assets_shares, class_name: 'Image', dependent: :destroy
   
 end
