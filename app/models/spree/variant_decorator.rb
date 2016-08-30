b89d33049294fa989ebcd5d90ad00601c20c823b
chung.triniti@gmail.com <johndoe@example.com>
2016-08-30 14:04:06 +0800

    Fix spree_variant

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -2,6 +2,5 @@ Spree::Variant.class_eval do
 
   has_many :assets_shares, -> { includes(:asset) }, dependent: :destroy, as: :shareable
   has_many :images, -> { order('assets.position ASC') }, source: :asset, foreign_key: 'asset_id', through: :assets_shares, class_name: 'Image', dependent: :destroy
-    
-  self.table_name = 'spree_variants'  
+  
 end
