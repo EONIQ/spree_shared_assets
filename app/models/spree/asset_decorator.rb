a69ed337e345180d10eab1723f95eb1c46c1c88f
chung.triniti@gmail.com <johndoe@example.com>
2016-08-30 15:13:50 +0800

    Fix select query for spree module

--- app/models/spree/asset_decorator.rb
+++ app/models/spree/asset_decorator.rb
@@ -10,7 +10,7 @@ Spree::Asset.class_eval do
   
   scope :for_product, ->(product) {
     joins(:variants, :assets_shares).
-    select('DISTINCT assets.*, assets_shares.shareable_id as shareable_id').
+    select('DISTINCT spree_assets.*, spree_assets_shares.shareable_id as shareable_id').
     where(spree_variants: { product_id: product.id }).order('spree_assets.position ASC') 
   }
 end
