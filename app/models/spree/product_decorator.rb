789abd15813e3e3c848993311aa27eab68f588b5
chung.triniti@gmail.com <johndoe@example.com>
2016-08-30 14:18:33 +0800

    Fix order in spree_assets

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Product.class_eval do 
   has_many :assets_shares, -> {includes(:asset)}, dependent: :destroy, as: :shareable
-  has_many :images, -> {order('assets.position ASC')}, source: :asset, foreign_key: 'asset_id', through: :assets_shares, class_name: 'Image'
+  has_many :images, -> {order('spree_assets.position ASC')}, source: :asset, foreign_key: 'asset_id', through: :assets_shares, class_name: 'Image'
   
   def all_images
     return images unless has_variants?
