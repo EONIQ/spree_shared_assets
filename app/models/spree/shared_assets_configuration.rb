a4e6d900351859580a7a1ba9a53b93547180c09c
chung.triniti@gmail.com <johndoe@example.com>
2016-09-14 19:56:08 +0800

    Fix configuration

--- app/models/spree/shared_assets_configuration.rb
+++ app/models/spree/shared_assets_configuration.rb
@@ -1,4 +1,4 @@
-class SharedAssetsConfiguration < Configuration
+class SharedAssetsConfiguration < Preferences::Configuration
   preference :admin_style, :string, :default => 'mini'
   preference :cropbox_width,  :integer, :default => 0
   preference :cropbox_height, :integer, :default => 0
