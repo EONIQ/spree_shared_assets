c8161b9cd8896c10723a3873438c0fc5a2435d7b
chung.triniti@gmail.com <johndoe@example.com>
2017-07-10 12:15:21 +0800

    Add back temp remove for 3-2 branch

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Variant.class_eval do
 
-  # has_many :assets_shares, -> { includes(:asset) }, dependent: :destroy, as: :shareable
-  # has_many :images, -> { order('spree_assets.position ASC') }, source: :asset, foreign_key: 'asset_id', through: :assets_shares, class_name: 'Spree::Image', dependent: :destroy
+  has_many :assets_shares, -> { includes(:asset) }, dependent: :destroy, as: :shareable
+  has_many :images, -> { order('spree_assets.position ASC') }, source: :asset, foreign_key: 'asset_id', through: :assets_shares, class_name: 'Spree::Image', dependent: :destroy
 
 end
